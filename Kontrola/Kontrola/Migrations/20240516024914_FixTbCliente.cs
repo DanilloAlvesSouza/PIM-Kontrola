28670a0dbc722a485b3a37547b6b0b3c4e3079b4
DanilloAlvesSouza <danillo.alves.souza@outlook.com>
2024-05-15 23:53:35 -0300

    Fix table db Cliente

---
+++ Kontrola/Kontrola/Migrations/20240516024914_FixTbCliente.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kontrola.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTbCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bairro",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Cep",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Cidade",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "Logradouro",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "UF",
+                table: "Cliente");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "Cliente",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelefoneDois",
+                table: "Cliente",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "Cliente",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "TelefoneDois",
+                table: "Cliente");
+
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "Cliente");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Bairro",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cep",
+                table: "Cliente",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cidade",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Logradouro",
+                table: "Cliente",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UF",
+                table: "Cliente",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
