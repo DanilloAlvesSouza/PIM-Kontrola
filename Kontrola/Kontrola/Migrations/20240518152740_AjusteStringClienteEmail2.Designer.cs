759e822707fce94492d5f7c348634edb7c51a73f
DanilloAlvesSouza <danillo.alves.souza@outlook.com>
2024-05-18 12:31:07 -0300

    fix db table equipamento

--- Kontrola/Kontrola/Migrations/20240518152740_AjusteStringClienteEmail2.Designer.cs
+++ Kontrola/Kontrola/Migrations/20240518152740_AjusteStringClienteEmail2.Designer.cs
@@ -4,6 +4,7 @@ using Kontrola.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kontrola.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240518152740_AjusteStringClienteEmail2")]
+    partial class AjusteStringClienteEmail2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
