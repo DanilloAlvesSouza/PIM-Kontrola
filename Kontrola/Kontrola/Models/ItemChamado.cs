5dde2ed25ff82d7677fe6c5ed9227e68f7fdb859
DanilloAlvesSouza <danillo.alves.souza@outlook.com>
2024-05-18 12:25:11 -0300

    Fix db e tela Create Equipamento

--- Kontrola/Kontrola/Models/ItemChamado.cs
+++ Kontrola/Kontrola/Models/ItemChamado.cs
@@ -2,8 +2,7 @@
 {
     public class ItemChamado
     {
-        public int ItemChamadoId { get; set; }
-        public int ChamadoId { get; set; }
+        public string ItemChamadoId { get; set; }
         public virtual Chamado Chamado { get; set; }
         public int EquipamentoId { get; set; }
         public virtual Equipamento Equipamento { get; set; }
